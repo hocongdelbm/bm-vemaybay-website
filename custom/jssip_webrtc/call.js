70479d82b0a033840a801f0e1990b10d00569851
haihung <hungnh12062000@gmail.com>
2024-07-25 09:18:50 +0700

    add hotlinr 4455

--- custom/jssip_webrtc/call.js
+++ custom/jssip_webrtc/call.js
@@ -1532,6 +1532,7 @@ $(function () {
                             <option value="0963987527@103.232.121.103:55000">0963987527</option>
                             <option value="0964031020@103.232.121.103:55000">0964031020</option>
                             <option value="0964359785@103.232.121.103:55000">0964359785</option>
+                            <option value="0968304455@103.232.121.103:55000">0968304455</option>
                         </optgroup>
                         <optgroup label="Mobiphone">
                             <option value="0933625233@103.199.78.74:65000">0933625233</option>
