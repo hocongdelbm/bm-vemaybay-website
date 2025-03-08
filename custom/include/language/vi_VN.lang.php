9331923bc50c4d218d7fae78d9fdbb6e5f9275cc
Hoang Duc <phamhoangduc10@gmail.com>
2025-03-08 15:32:32 +0700

    Add label to language

--- custom/include/language/vi_VN.lang.php
+++ custom/include/language/vi_VN.lang.php
@@ -89,6 +89,7 @@ $app_list_strings['moduleList']['EC_LeaveAbsences']       = 'Nghỉ phép';
 $app_list_strings['moduleList']['EC_LyDoThangThua']       = 'Lý do thắng thua';
 $app_list_strings['moduleList']['EC_Location']            = 'Địa điểm';
 $app_list_strings['moduleList']['EC_LoginAudit']          = 'Login Audit';
+$app_list_strings['moduleList']['EC_Contact_Points_Log']  = 'Quá trình tích điểm';
 // AOR
 $app_list_strings['moduleList']['AOR_Reports'] = 'Báo cáo';
 $app_list_strings['moduleList']['AOR_Conditions'] = 'Báo cáo điều kiện';
