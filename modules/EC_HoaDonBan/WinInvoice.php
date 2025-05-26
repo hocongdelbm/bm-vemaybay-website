e6c1c91ddea555afadecaffc00e29a5eccca9034
ducphamlbm <ducpham@lebaominh.com>
2025-05-26 08:41:06 +0700

    Sync code from server to git

--- modules/EC_HoaDonBan/WinInvoice.php
+++ modules/EC_HoaDonBan/WinInvoice.php
@@ -414,15 +414,13 @@ class WinInvoice extends InvoiceLogs
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 20,
+            CURLOPT_TIMEOUT => 30,
             CURLOPT_CUSTOMREQUEST => 'GET',
         ));
         $json = curl_exec($curl);
         $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         curl_close($curl);
-
-        if ($httpcode == 200) return $json;
-        return null;
+        return $json;
     }
 
 
