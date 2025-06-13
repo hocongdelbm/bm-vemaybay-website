51cfb1d7d282b76616da1f7e87a791deccf63fda
HaiVH07 <haivh@lebaominh.com>
2025-06-13 13:57:04 +0700

    update tpl

--- modules/EC_Flight_Bookings/views/view.clientphonetcb.php
+++ modules/EC_Flight_Bookings/views/view.clientphonetcb.php
@@ -4,240 +4,6 @@ require_once("include/Sugar_Smarty.php");
 class Viewclientphonetcb extends SugarView
 {
 
-    // function display()
-    // {
-    //     $this->displayStyle();
-    //     $smarty = new Sugar_Smarty();
-
-
-    //     function convertToYMD($dateStr)
-    //     {
-    //         $parts = explode('-', $dateStr); // dd-mm-yyyy
-    //         if (count($parts) === 3) {
-    //             return $parts[2] . '-' . $parts[1] . '-' . $parts[0];
-    //         }
-    //         return '';
-    //     }
-
-    //     // Get date filters from GET parameters
-    //     $fromDate = isset($_GET['from_date']) ? $_GET['from_date'] : '';
-    //     $toDate   = isset($_GET['to_date']) ? $_GET['to_date'] : '';
-    //     $smarty->assign('from_date', $fromDate);
-    //     $smarty->assign('to_date', $toDate);
-
-    //     // Convert to Y-m-d for filtering
-    //     $fromYMD = !empty($fromDate) ? convertToYMD($fromDate) : '';
-    //     $toYMD   = !empty($toDate)   ? convertToYMD($toDate)   : '';
-    //     $fromTimestamp = !empty($fromYMD) ? strtotime($fromYMD) : null;
-    //     $toTimestamp   = !empty($toYMD)   ? strtotime($toYMD)   : '';
-
-    //     // Get the source from GET parameter (defaults to first source if not set)
-    //     $source = isset($_GET['source']) ? $_GET['source'] : 'timchuyenbay.com';
-    //     $smarty->assign('source', $source);
-
-    //     // Array of possible sources
-    //     $sources = ['timchuyenbay.vn', 'vietjet.net', 'timchuyenbay.com'];
-
-    //     $data = [];
-
-    //     foreach ($sources as $currentSource) {
-    //         if ($currentSource === $source) {
-    //             $page = isset($_GET['page']) && is_numeric($_GET['page']) ? intval($_GET['page']) : 1;
-    //             $perPage = 50;
-    //             $offset = ($page - 1) * $perPage;
-
-    //             $responseData = $this->getDataFromTicketWebsite($currentSource, $fromDate, $toDate, $offset, $perPage, 0);  // offset, limit, is_used
-
-    //             if (isset($responseData['message']) && $responseData['message'] === 'Success') {
-    //                 $data = array_merge($data, $responseData['data']);
-    //             }
-    //         }
-    //     }
-
-    //     global $db;
-    //     $zaloZnsQuery = "
-    //         SELECT send_to, id
-    //         FROM ec_messages
-    //         WHERE type = 'zalo_zns'
-    //         AND category = 'customer_care'
-    //         AND parent_type != 'EC_Flight_Bookings'
-    //     ";
-    //     $zaloZnsRes = $db->query($zaloZnsQuery);
-    //     $zaloZnsMap = [];
-    //     while ($row = $db->fetchByAssoc($zaloZnsRes)) {
-    //         // Map send_to (phone_number) to its corresponding zns_id
-    //         $zaloZnsMap[$row['send_to']] = $row['id'];
-    //     }
-
-    //     // Get Calls Data
-    //     $minDateEntered = null;
-    //     foreach ($data as $entry) {
-    //         $currentDate = strtotime($entry['date_entered']);
-    //         if ($minDateEntered === null || $currentDate < $minDateEntered) {
-    //             $minDateEntered = $currentDate;
-    //         }
-    //     }
-    //     $minDateAdjusted = $minDateEntered - 7 * 3600;
-
-    //     $query = "
-    //         SELECT call_to, id, date_modified
-    //         FROM calls
-    //         WHERE deleted = 0
-    //         AND date_modified > '" . date('Y-m-d H:i:s', $minDateAdjusted) . "'
-    //     ";
-
-    //     $res = $db->query($query);
-    //     $callMap = [];
-    //     while ($row = $db->fetchByAssoc($res)) {
-    //         if (!empty($row['call_to'])) {
-    //             $phone = $row['call_to'];
-    //             $dateModified = strtotime($row['date_modified']);
-    //             if (!isset($callMap[$phone]) || $dateModified > strtotime($callMap[$phone]['date_modified'])) {
-    //                 $callMap[$phone] = [
-    //                     'id' => $row['id'],
-    //                     'date_modified' => $row['date_modified'],
-    //                 ];
-    //             }
-    //         }
-    //     }
-    //     $callCheck = isset($_GET['call_recheck']) && $_GET['call_recheck'] == 'on';
-    //     $smarty->assign('callCheck', $callCheck);
-
-    //     $bookingIds = array_column($data, 'booking_id');
-    //     $bookingIds = array_filter($bookingIds);
-
-    //     if (!empty($bookingIds)) {
-    //         $bookingIdsStr = "'" . implode("', '", $bookingIds) . "'";
-
-    //         $query = "
-    //             SELECT id, name
-    //             FROM ec_flight_bookings
-    //             WHERE id IN ($bookingIdsStr)
-    //         ";
-
-    //         // Execute the query
-    //         $bookingRes = $db->query($query);
-
-    //         // Initialize the bookingMap array
-    //         $bookingMap = [];
-
-    //         // Fetch the results from the query result object
-    //         while ($booking = $db->fetchByAssoc($bookingRes)) {
-    //             // Map booking id to booking name
-    //             $bookingMap[$booking['id']] = $booking['name'];
-    //         }
-
-    //         foreach ($data as &$entry) {
-    //             if (!empty($entry['booking_id']) && isset($bookingMap[$entry['booking_id']])) {
-    //                 $entry['booking_name'] = $bookingMap[$entry['booking_id']];
-    //             } else {
-    //                 $entry['booking_name'] = '';
-    //             }
-    //         }
-    //     } else {
-    //         foreach ($data as &$entry) {
-    //             $entry['booking_name'] = '';
-    //         }
-    //     }
-
-    //     foreach ($data as &$entry) {
-    //         $entry['is_zns'] = isset($zaloZnsMap[$entry['phone_number']]) ? true : false;
-
-    //         if ($entry['is_zns']) {
-    //             $entry['zns_id'] = $zaloZnsMap[$entry['phone_number']];
-    //         }
-
-    //         $entryDateAdjusted = strtotime($entry['date_entered']) - 7 * 3600;
-
-    //         $phone = $entry['phone_number'];
-    //         if (isset($callMap[$phone])) {
-    //             $callDate = strtotime($callMap[$phone]['date_modified']);
-    //             $entry['in_calls'] = $entryDateAdjusted < $callDate;
-    //             $entry['call_id'] = $entry['in_calls'] ? $callMap[$phone]['id'] : '';
-    //         } else {
-    //             $entry['in_calls'] = false;
-    //             $entry['call_id'] = '';
-    //         }
-
-    //         $entry['source'] = isset($source) ? $source : 'Unknown';
-    //     }
-
-    //     if ($callCheck) {
-    //         $data = array_filter($data, function ($entry) {
-    //             return !$entry['in_calls']; // Only include entries where in_calls is false
-    //         });
-    //     }
-    //     unset($entry);
-
-    //     if ($fromTimestamp || $toTimestamp) {
-    //         $data = array_filter($data, function ($item) use ($fromTimestamp, $toTimestamp) {
-    //             $itemDate = strtotime(substr($item['date_entered'], 0, 10)); // 'YYYY-MM-DD'
-    //             return (!$fromTimestamp || $itemDate >= $fromTimestamp) &&
-    //                 (!$toTimestamp   || $itemDate <= $toTimestamp);
-    //         });
-
-    //         $data = array_values($data);
-    //     }
-
-    //     $page = isset($_GET['page']) && is_numeric($_GET['page']) ? intval($_GET['page']) : 1;
-    //     $perPage = 50;
-    //     $totalRows = count($data);
-    //     $totalPages = ceil($totalRows / $perPage);
-    //     $start = ($page - 1) * $perPage;
-    //     $pagedData = array_slice($data, $start, $perPage);
-
-    //     $baseUrl = 'index.php?module=EC_Flight_Bookings&action=clientphonetcb';
-
-    //     if ($fromDate !== '' && $fromDate !== null) {
-    //         $baseUrl .= '&from_date=' . urlencode($fromDate);
-    //     }
-    //     if ($toDate !== '' && $toDate !== null) {
-    //         $baseUrl .= '&to_date=' . urlencode($toDate);
-    //     }
-
-    //     if (!empty($source))   $baseUrl .= '&source=' . urlencode($source);
-    //     if (isset($_GET['call_recheck']) && $_GET['call_recheck'] == 'on') {
-    //         $baseUrl .= '&call_recheck=on';
-    //     }
-    //     $pageData = [
-    //         'urls' => [
-    //             'startPage' => $page > 1 ? $baseUrl . '&page=1' : '',
-    //             'prevPage'  => $page > 1 ? $baseUrl . '&page=' . ($page - 1) : '',
-    //             'nextPage'  => $page < $totalPages ? $baseUrl . '&page=' . ($page + 1) : '',
-    //             'endPage'   => $page < $totalPages ? $baseUrl . '&page=' . $totalPages : '',
-    //         ],
-    //         'offsets' => [
-    //             'current' => $start,
-    //             'lastOffsetOnPage' => $start + count($pagedData),
-    //             'total' => $totalRows,
-    //             'totalCounted' => true,
-    //             'next' => min($start + $perPage, $totalRows),
-    //             'prev' => max(0, $start - $perPage),
-    //         ],
-    //     ];
-
-    //     // Navigation labels for pagination
-    //     $navStrings = [
-    //         'start' => 'Trang đầu',
-    //         'previous' => 'Trước',
-    //         'next' => 'Tiếp theo',
-    //         'end' => 'Trang cuối',
-    //         'of' => 'trên tổng số',
-    //     ];
-
-    //     // Assign the processed data and other variables to Smarty
-    //     $smarty->assign('phone_data', $pagedData);
-    //     $smarty->assign('pageData', $pageData);
-    //     $smarty->assign('navStrings', $navStrings);
-    //     $smarty->assign('action_menu_location', 'bottom');
-    //     $smarty->assign('prerow', false);
-    //     $smarty->assign('colCount', 9);  // Update column count for new source column
-
-    //     // Display the template
-    //     $smarty->display('modules/' . $this->bean->module_dir . '/tpls/view_phone_request_tcb.tpl');
-    //     $this->displayScript();
-    // }
-
     function display()
     {
         $this->displayStyle();
@@ -265,18 +31,14 @@ class Viewclientphonetcb extends SugarView
         $fromTimestamp = !empty($fromYMD) ? strtotime($fromYMD) : null;
         $toTimestamp = !empty($toYMD) ? strtotime($toYMD) : '';
 
-        // Get the source from GET parameter (defaults to 'timchuyenbay.com' if not set)
         $source = isset($_GET['source']) ? $_GET['source'] : 'timchuyenbay.com';
         $smarty->assign('source', $source);
 
-        // Possible sources
         $sources = ['timchuyenbay.vn', 'vietjet.net', 'timchuyenbay.com'];
 
-        // Get the current page and calculate offset
         $page = isset($_GET['page']) && is_numeric($_GET['page']) ? intval($_GET['page']) : 1;
-        $perPage = 50;  // Number of items per page
-        $offset = ($page - 1) * $perPage;  // Calculate offset based on page
-
+        $perPage = 50; 
+        $offset = ($page - 1) * $perPage;
         $data = [];
 
         // Fetch data from the API
@@ -290,7 +52,7 @@ class Viewclientphonetcb extends SugarView
             }
         }
 
-        // Zalo ZNS Mapping
+        // Zalo ZNS
         global $db;
         $zaloZnsQuery = "
             SELECT send_to, id
@@ -335,7 +97,7 @@ class Viewclientphonetcb extends SugarView
                 }
             }
         }
-
+        // check recall
         $callCheck = isset($_GET['call_recheck']) && $_GET['call_recheck'] == 'on';
         $smarty->assign('callCheck', $callCheck);
 
@@ -363,7 +125,7 @@ class Viewclientphonetcb extends SugarView
                 if (!empty($entry['booking_id']) && isset($bookingMap[$entry['booking_id']])) {
                     $entry['booking_name'] = $bookingMap[$entry['booking_id']];
                 } else {
-                    $entry['booking_name'] = '';  // If no name found
+                    $entry['booking_name'] = '';
                 }
             }
         }
@@ -394,7 +156,7 @@ class Viewclientphonetcb extends SugarView
         // Filter by call recheck checkbox
         if ($callCheck) {
             $data = array_filter($data, function ($entry) {
-                return !$entry['in_calls']; // Only include entries where in_calls is false
+                return !$entry['in_calls'];
             });
         }
 
@@ -496,7 +258,7 @@ class Viewclientphonetcb extends SugarView
         }
 
         if ($fromDate === '') {
-            $fromDate = date('d-m-Y');
+             $fromDate = date('d-m-Y', strtotime('-30 days'));
         }
         if ($toDate === '') {
             $toDate = date('d-m-Y');
@@ -508,7 +270,7 @@ class Viewclientphonetcb extends SugarView
             'from_date' => $fromDate,
             'to_date' => $toDate,
             'offset' => $offset,
-            'limit' => 10000,
+            'limit' => 50,
             'is_used' => $isUsed
         ]);
 
@@ -519,12 +281,12 @@ class Viewclientphonetcb extends SugarView
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30, // Increase timeout if necessary
+            CURLOPT_TIMEOUT => 60,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_HTTPHEADER => [
-                'Api-Key: 4F3yBy83DIRuHaFp6e@alBkS-sb3T3)kvZ8$-MqM',  // Add your actual API key here
+                'Api-Key: 4F3yBy83DIRuHaFp6e@alBkS-sb3T3)kvZ8$-MqM', 
                 'Content-Type: application/json',
             ],
             CURLOPT_POSTFIELDS => $data,
@@ -549,85 +311,10 @@ class Viewclientphonetcb extends SugarView
 
         $responseData = json_decode($response, true);
 
-        // Check for JSON decode errors
         if (json_last_error() !== JSON_ERROR_NONE) {
             return false;
         }
 
         return $responseData;
     }
-    // function getDataFromTicketWebsite($source, $fromDate, $toDate, $offset = 0, $limit = 50, $isUsed = 0)
-    // {
-    //     // Prepare the URL based on the source
-    //     $url = '';
-    //     if ($source === 'timchuyenbay.vn') {
-    //         $url = 'https://timchuyenbay.vn/api';
-    //     } elseif ($source === 'vietjet.net') {
-    //         $url = 'https://vietjet.net/api';
-    //     } elseif ($source === 'timchuyenbay.com') {
-    //         $url = 'https://timchuyenbay.com/api';
-    //     } else {
-    //         $url = 'https://vietjet.net/api'; // Default fallback
-    //     }
-    //     if ($fromDate === '') {
-    //         $fromDate = date('d-m-Y');
-    //     }
-    //     if ($toDate === '') {
-    //         $toDate = date('d-m-Y');
-    //     }
-    //     // Prepare request payload with dynamic parameters
-    //     $data = json_encode([
-    //         'action' => 'get_list_phone_request',
-    //         'from_date' => $fromDate,
-    //         'to_date' => $toDate,
-    //         'offset' => $offset,
-    //         'limit' => $limit,
-    //         'is_used' => $isUsed
-    //     ]);
-
-    //     $curl = curl_init();
-
-    //     curl_setopt_array($curl, [
-    //         CURLOPT_URL => $url,
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_ENCODING => '',
-    //         CURLOPT_MAXREDIRS => 10,
-    //         CURLOPT_TIMEOUT => 30, // Increase timeout if necessary
-    //         CURLOPT_FOLLOWLOCATION => true,
-    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //         CURLOPT_CUSTOMREQUEST => 'POST',
-    //         CURLOPT_HTTPHEADER => [
-    //             'Api-Key: 4F3yBy83DIRuHaFp6e@alBkS-sb3T3)kvZ8$-MqM',
-    //             'Content-Type: application/json',
-    //         ],
-    //         CURLOPT_POSTFIELDS => $data,
-    //         CURLOPT_SSL_VERIFYPEER => false,
-    //         CURLOPT_SSL_VERIFYHOST => false,
-    //     ]);
-
-    //     $response = curl_exec($curl);
-
-    //     if (curl_errno($curl)) {
-    //         echo 'cURL error: ' . curl_error($curl);
-    //         curl_close($curl);
-    //         return false;
-    //     }
-
-    //     curl_close($curl);
-
-    //     if (!$response) {
-    //         echo "No response from API.";
-    //         return false;
-    //     }
-
-    //     $responseData = json_decode($response, true);
-
-    //     // Check for JSON decode errors
-    //     if (json_last_error() !== JSON_ERROR_NONE) {
-    //         // echo "JSON decode error: " . json_last_error_msg();
-    //         return false;
-    //     }
-
-    //     return $responseData;
-    // }
 }
