a041226815921487cc7f7e31545187a9f72c3042
ducphamlbm <ducpham@lebaominh.com>
2025-06-18 11:43:18 +0700

    Fix function createContactsForBooking

--- custom/include/utils/booking.php
+++ custom/include/utils/booking.php
@@ -11,6 +11,7 @@ function createContactsForBooking($phoneNumber, $contactName = '')
     global $db, $current_user;
 
     $phoneNumber = trim($phoneNumber);
+    if(!$phoneNumber && empty($phoneNumber)) return false;
 
     $sql_contact = "SELECT id FROM contacts WHERE phone_mobile = '$phoneNumber' AND deleted = 0";
     $contact_id = $db->getOne($sql_contact);
@@ -38,8 +39,10 @@ function createContactsForBooking($phoneNumber, $contactName = '')
                 LIMIT 1";
             $get_contact_name = $db->getOne($sql_booking);
 
-            $contact->last_name = $get_contact_name;
-            $contact->save();
+            if($get_contact_name && !empty($get_contact_name)) {
+                $contact->last_name = $get_contact_name;
+                $contact->save();
+            }
         }
     }
 
