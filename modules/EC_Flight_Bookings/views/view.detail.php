65ce9f1cab9343e15fa82ed9eb723cbeb97f3414
ducphamlbm <ducpham@lebaominh.com>
2025-08-08 23:24:30 +0700

    Fix print and send eticket

--- modules/EC_Flight_Bookings/views/view.detail.php
+++ modules/EC_Flight_Bookings/views/view.detail.php
@@ -66,7 +66,7 @@ class EC_Flight_BookingsViewDetail extends ViewDetail
 		global $app_list_strings, $current_user;
 
 		// External file
-		$js = '<script src="modules/' . $this->bean->module_dir . '/js/view.detail.js?v=1.4.5"></script>
+		$js = '<script src="modules/' . $this->bean->module_dir . '/js/view.detail.js?v=1.4.6"></script>
 			<script src="modules/' . $this->bean->module_dir . '/js/api_phuongnam/booking.js?v=1.2"></script>
 			<script src="modules/' . $this->bean->module_dir . '/js/api_zalo.js?v=1.9"></script>
 			<script src="modules/' . $this->bean->module_dir . '/js/api_sms.js?v=1.3"></script>
