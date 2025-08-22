619bd1e97db7ebf6b7edfb74521adb2443108451
ducphamlbm <ducpham@lebaominh.com>
2025-08-22 14:33:25 +0700

    Fix show QR code

--- modules/EC_Flight_Bookings/views/view.detail.php
+++ modules/EC_Flight_Bookings/views/view.detail.php
@@ -637,7 +637,8 @@ class EC_Flight_BookingsViewDetail extends ViewDetail
 		else $payment_link = "$server_name/thanh-toan-online?paymentlink=". $this->bean->nganluong_code ."&datepaid=$datepaid";
 		$array_servername = [
 			'vietjet.net',
-			'timchuyenbay.com'
+			'timchuyenbay.com',
+			'timchuyenbay.vn'
 		];
 
 		if (in_array($server_name, $array_servername)) {
@@ -683,57 +684,72 @@ class EC_Flight_BookingsViewDetail extends ViewDetail
 										</div>
 									</div>
 								</div>';
-			} else {
+			}
+			else {
 				// Lịch sử giao dịch
 				$nganluong_info = json_decode(html_entity_decode($this->bean->nganluong_info), true);
 
 				$nganluong_code .= '<div class="modal fade" id="history-transaction" tabindex="-1" aria-labelledby="history-transactionLabel" aria-hidden="true">
-										<div class="modal-dialog modal-dialog-centered">
-											<div class="modal-content">
-												<div class="modal-header">
-													<h2 class="modal-title fs-5" id="history-transactionLabel">Lịch sử giao dịch Onepay</h2>
-													<button type="button" class="btn-close d-flex align-items-center" data-bs-dismiss="modal" aria-label="Close">
-														<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-															<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-														</svg>
-													</button>
-												</div><div class="modal-body d-flex align-items-center gap-3 justify-content-center flex-column">';
-
+					<div class="modal-dialog modal-dialog-centered">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h2 class="modal-title fs-5" id="history-transactionLabel">Lịch sử giao dịch Onepay</h2>
+								<button type="button" class="btn-close d-flex align-items-center" data-bs-dismiss="modal" aria-label="Close">
+									<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+										<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+									</svg>
+								</button>
+							</div>
+							<div class="modal-body d-flex align-items-center gap-3 justify-content-center flex-column">';
 				foreach ($nganluong_info as $val) {
 					$nganluong_code .= '<div class="history-card">
-																	<div class="history-line time-transaction">
-																		<div class="transaction-label">Thời gian</div>
-																		<div class="transaction-value">' . date('H:i:s d/m/Y', strtotime('+7 hours', strtotime($val['payment_date']))) . '</div>
-																	</div>
-																	<div class="history-line code-transaction">
-																		<div class="transaction-label">Mã giao dịch</div>
-																		<div class="transaction-value">' . $val['vpc_TransactionNo'] . '</div>
-																	</div>
-																	<div class="history-line amount-transaction">
-																		<div class="transaction-label">Số tiền thanh toán</div>
-																		<div class="transaction-value">' . format_number(substr($val['vpc_Amount'], 0, -2)) . ' VND</div>
-																	</div>
-																	<div class="history-line fee-transaction">
-																		<div class="transaction-label">Phí giao dịch</div>
-																		<div class="transaction-value">Miễn phí</div>
-																	</div>
-																	<div class="history-line desc-transaction">
-																		<div class="transaction-label">Nội dung</div>
-																		<div class="transaction-value">' . $val['payment_note'] . '</div>
-																	</div>
-																</div>';
+						<div class="history-line time-transaction">
+							<div class="transaction-label">Thời gian</div>
+							<div class="transaction-value">' . date('H:i:s d/m/Y', strtotime('+7 hours', strtotime($val['payment_date']))) . '</div>
+						</div>
+						<div class="history-line code-transaction">
+							<div class="transaction-label">Mã giao dịch</div>
+							<div class="transaction-value">' . $val['vpc_TransactionNo'] . '</div>
+						</div>
+						<div class="history-line amount-transaction">
+							<div class="transaction-label">Số tiền thanh toán</div>
+							<div class="transaction-value">' . format_number(substr($val['vpc_Amount'], 0, -2)) . ' VND</div>
+						</div>
+						<div class="history-line fee-transaction">
+							<div class="transaction-label">Phí giao dịch</div>
+							<div class="transaction-value">Miễn phí</div>
+						</div>
+						<div class="history-line desc-transaction">
+							<div class="transaction-label">Nội dung</div>
+							<div class="transaction-value">' . $val['payment_note'] . '</div>
+						</div>
+					</div>';
 				}
-
-				$nganluong_code .= '</div><div class="modal-footer">
-													<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
-												</div>
-											</div>
-										</div>
-									</div>';
+				$nganluong_code .= '</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+							</div>
+						</div>
+					</div>
+				</div>';
 			}
 
 			$nganluong_code .= $this->generateDialogGetQRCode($this->bean->total_amount, $this->bean->phone);
-		};
+		}
+		else {
+			$nganluong_code = '<div class="nganluong__wrap">
+				<button class="flex-fill outline-none" id="get_qr_code">
+					<svg width="20px" height="20px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M9 6.6V8.4C9 8.73137 8.73137 9 8.4 9H6.6C6.26863 9 6 8.73137 6 8.4V6.6C6 6.26863 6.26863 6 6.6 6H8.4C8.73137 6 9 6.26863 9 6.6Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6 12H9" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M15 12V15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 18H15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 12.0111L12.01 12" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 12.0111L18.01 12" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 15.0111L12.01 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 15.0111L18.01 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 18.0111L18.01 18" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 9.01111L12.01 9" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 6.01111L12.01 6" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9 15.6V17.4C9 17.7314 8.73137 18 8.4 18H6.6C6.26863 18 6 17.7314 6 17.4V15.6C6 15.2686 6.26863 15 6.6 15H8.4C8.73137 15 9 15.2686 9 15.6Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 6.6V8.4C18 8.73137 17.7314 9 17.4 9H15.6C15.2686 9 15 8.73137 15 8.4V6.6C15 6.26863 15.2686 6 15.6 6H17.4C17.7314 6 18 6.26863 18 6.6Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 3H21V6" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18 21H21V18" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6 3H3V6" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6 21H3V18" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+					<span>QR code</span>
+				</button>
+				<button class="flex-fill outline-none btn btn-primary-2" id="get_bank" booking_id="' . $this->bean->id . '">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bank" viewBox="0 0 16 16">
+						<path d="m8 0 6.61 3h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.38l.5 2a.498.498 0 0 1-.485.62H.5a.498.498 0 0 1-.485-.62l.5-2A.5.5 0 0 1 1 13V6H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 3h.89zM3.777 3h8.447L8 1zM2 6v7h1V6zm2 0v7h2.5V6zm3.5 0v7h1V6zm2 0v7H12V6zM13 6v7h1V6zm2-1V4H1v1zm-.39 9H1.39l-.25 1h13.72z"/>
+					</svg>
+					<span>Ngân hàng</span>
+				</button>
+			</div>';
+		} 
 		$this->ss->assign('CUSTOM_NGANLUONG_CODE', $nganluong_code);
 
 		// Assigned to name (giao cho)
