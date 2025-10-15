e369a293f62f1856baf7b39a2049a03a3b8519f1
ducphamlbm <ducpham@lebaominh.com>
2025-10-15 16:54:47 +0700

    Update version css, js

--- modules/EC_Flight_Bookings/views/view.updateticket.php
+++ modules/EC_Flight_Bookings/views/view.updateticket.php
@@ -11,12 +11,12 @@ class Viewupdateticket extends SugarView {
 
 	function displayStyle() {
 		$style = '';
-		$style .= '<link type="text/css" rel="stylesheet" href="modules/'.$this->bean->module_dir.'/css/updateticket.css?v=1.0.0">';
+		$style .= '<link type="text/css" rel="stylesheet" href="modules/'.$this->bean->module_dir.'/css/updateticket.css?v=1.0.1">';
         echo $style;
     }
     function displayScript() {
 		$script = '';
-		$script .= '<script src="modules/'.$this->bean->module_dir.'/js/updateticket.js?v=1.0.0"></script>';
+		$script .= '<script src="modules/'.$this->bean->module_dir.'/js/updateticket.js?v=1.0.1"></script>';
         echo $script;
     }
 }
