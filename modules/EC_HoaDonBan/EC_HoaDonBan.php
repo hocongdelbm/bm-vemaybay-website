c9aae5ff42c5d1d4d3386753c6caea9aed1c078e
ducphamlbm <ducpham@lebaominh.com>
2025-11-04 09:20:45 +0700

    Update dongia when auto create output invoice

--- modules/EC_HoaDonBan/EC_HoaDonBan.php
+++ modules/EC_HoaDonBan/EC_HoaDonBan.php
@@ -375,7 +375,7 @@ class EC_HoaDonBan extends Basic {
 						$outInvDetail->phidv 		= $serviceFee;
 						$outInvDetail->giamua 		= $tk['total'];
 						$outInvDetail->thuesuat 	= $taxRate;
-						$outInvDetail->dongia 		= ($outInvDetail->giamua + $serviceFee - $outInvDetail->phithuho) / $divide;
+						$outInvDetail->dongia 		= (($outInvDetail->giamua / $tk['qty']) + $serviceFee - $outInvDetail->phithuho) / $divide;
 						$outInvDetail->tienthue 	= $outInvDetail->dongia * $taxRate * $tk['qty'];
 						$outInvDetail->thanhtien 	= ($outInvDetail->dongia + $outInvDetail->tienthue + $outInvDetail->phithuho) * $outInvDetail->soluong;
 						$outInvDetail->parent_id 	= $parentId;
