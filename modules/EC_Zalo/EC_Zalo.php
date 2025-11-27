6973a20009476f14c051fe64426947722469d2cb
ducphamlbm <ducpham@lebaominh.com>
2025-11-27 10:57:58 +0700

    Sync code from server to github

--- modules/EC_Zalo/EC_Zalo.php
+++ modules/EC_Zalo/EC_Zalo.php
@@ -82,7 +82,7 @@ class EC_Zalo extends Basic {
         $dbInfo = $this->db->fetchByAssoc($res);
 
         $api_oauth_info = json_decode(html_entity_decode($dbInfo['api_oauth_info']), true);
-        if($api_oauth_info['error'] == -14014 || $api_oauth_info['error'] == -216) return $api_oauth_info;
+        if(is_null($api_oauth_info) || empty($api_oauth_info) || !isset($api_oauth_info['access_token'])) return ['error' => -216];
 
         if(is_array($dbInfo) && !empty($dbInfo) && date('Y-m-d') == date('Y-m-d', strtotime($dbInfo['date_modified']))) {
             unset($dbInfo['date_modified']);
