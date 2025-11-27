d64a1211af074cd9509f3c3d01742966d417c6f2
ducphamlbm <ducpham@lebaominh.com>
2025-11-27 10:47:21 +0700

    Update auth zalo

--- modules/EC_Zalo/EC_Zalo.php
+++ modules/EC_Zalo/EC_Zalo.php
@@ -74,11 +74,15 @@ class EC_Zalo extends Basic {
                 ,package_auto_renew_date
                 ,linked_zca AS linked_ZCA
                 ,quota_info
+                ,api_oauth_info 
                 ,DATE_ADD(date_modified, INTERVAL 7 HOUR) AS date_modified
             FROM ec_zalo
             WHERE id = '{$oa_id}' AND deleted = 0";
         $res = $this->db->query($sql);
         $dbInfo = $this->db->fetchByAssoc($res);
+
+        $api_oauth_info = json_decode(html_entity_decode($dbInfo['quotaapi_oauth_info_info']), true);
+        if($api_oauth_info['error'] == '-14014' || $api_oauth_info['error'] == '-216') return $api_oauth_info;
 
         if(is_array($dbInfo) && !empty($dbInfo) && date('Y-m-d') == date('Y-m-d', strtotime($dbInfo['date_modified']))) {
             unset($dbInfo['date_modified']);
