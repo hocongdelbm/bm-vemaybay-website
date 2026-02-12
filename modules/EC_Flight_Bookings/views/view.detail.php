1b34a8adb4450ac77a709e80aee105990cb6f4d5
Van Dahy <vandahy@gmail.com>
2026-02-12 16:01:53 +0700

    edit systax code

--- modules/EC_Flight_Bookings/views/view.detail.php
+++ modules/EC_Flight_Bookings/views/view.detail.php
@@ -150,7 +150,7 @@ class EC_Flight_BookingsViewDetail extends ViewDetail
 			<script src="modules/' . $this->bean->module_dir . '/js/autobook.js?v=1.6"></script>
 			<script src="modules/' . $this->bean->module_dir . '/js/api_zalo.js?v=2.0"></script>
 			<script src="modules/' . $this->bean->module_dir . '/js/api_sms.js?v=1.3.2"></script>
-			<script src="modules/' . $this->bean->module_dir . '/js/doc_list.js?v=1.0"></script>';
+			<script src="modules/' . $this->bean->module_dir . '/js/doc_list.js?v=1.0"></script>
 			<script src="modules/' . $this->bean->module_dir . '/js/print_ticket.js?v=1.0"></script>
 			<script src="modules/' . $this->bean->module_dir . '/js/api_sms.js?v=1.3.2"></script>';
 
