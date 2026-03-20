10487da8bc6e4b387dfebc83d01523613bf3395e
ducphamlbm <ducpham@lebaominh.com>
2026-03-20 17:17:24 +0700

    Build feature maintaining zalo chat and NotificationService

--- modules/Schedulers/language/vi_VN.lang.php
+++ modules/Schedulers/language/vi_VN.lang.php
@@ -153,6 +153,7 @@ $mod_strings = [
     // AUTOCALL
     'LBL_UPDATELOGAUTOCALL' => 'Cập nhật log cuộc gọi tự động',
     'LBL_SENDAUTOCHEAPPRICEMESSAGEZALO' => 'Gửi tin tự động về giá vé rẻ qua ZBS template Zalo',
+    'LBL_MAINTAINZALOCHAT' => 'Tự động gửi tin tư vấn Zalo để duy trì tương tác',
 
     'LBL_SAVEREVENUEBOOKINGJOB' => 'Cập nhật doanh số booking Revenue',
     'LBL_NOTIFYCHECKINJOURNEY' => 'Thông báo hành trình cần checkin',
