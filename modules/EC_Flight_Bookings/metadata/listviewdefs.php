542a0533de786498fa08a0abe64b27be597431e1
ducphamlbm <ducpham@lebaominh.com>
2026-03-24 17:35:13 +0700

    Sync changed code on server to github

--- modules/EC_Flight_Bookings/metadata/listviewdefs.php
+++ modules/EC_Flight_Bookings/metadata/listviewdefs.php
@@ -72,7 +72,7 @@ $listViewDefs[$module_name] = array(
     'IP_ADDRESS' => array(
         'width' => '10%',
         'label' => 'LBL_IP_ADDRESS',
-        'default' => false,
+        'default' => true,
     ),
     'DATE_ENTERED' => array(
         'label' => 'LBL_DATE_ENTERED',
