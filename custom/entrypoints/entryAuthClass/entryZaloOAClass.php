230b5f11026da67fe0cb88e607b196d5f9542bdb
ducphamlbm <ducpham@lebaominh.com>
2026-03-26 17:43:49 +0700

    Fix use namespance service

--- custom/entrypoints/entryAuthClass/entryZaloOAClass.php
+++ custom/entrypoints/entryAuthClass/entryZaloOAClass.php
@@ -2,7 +2,7 @@
 require_once "custom/entrypoints/entryClass.php";
 require_once "custom/include/helpers/api/APIZaloOA.php";
 
-use Services\Notification\NotificationService;
+use \Custom\Services\Notification\NotificationService;
 
 /**
  * Class entryZaloOAClass
