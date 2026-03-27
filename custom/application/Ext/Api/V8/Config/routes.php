360338fec5ac5019fce1b2f186e69f33c5676a35
ducphamlbm <ducpham@lebaominh.com>
2026-03-27 23:48:47 +0700

    Create custom api save_location_booking

--- custom/application/Ext/Api/V8/Config/routes.php
+++ custom/application/Ext/Api/V8/Config/routes.php
@@ -6,6 +6,9 @@ require 'custom/application/Ext/Api/V8/Controller/CustomController.php';
 $app->post('/save_call', 'Api\V8\Controller\CustomController:save_call');
 $app->post('/save_booking', 'Api\V8\Controller\CustomController:save_booking');
 $app->post('/save_contacts', 'Api\V8\Controller\CustomController:save_contacts');
-$app->post('/get_info_voucher', 'Api\V8\Controller\CustomController:get_info_voucher');
-$app->post('/send_zns', 'Api\V8\Controller\CustomController:send_zns');
 $app->post('/save_voucher', 'Api\V8\Controller\CustomController:save_voucher');
+$app->post('/save_location_booking', 'Api\V8\Controller\CustomController:save_location_booking');
+
+$app->post('/get_info_voucher', 'Api\V8\Controller\CustomController:get_info_voucher');
+
+$app->post('/send_zns', 'Api\V8\Controller\CustomController:send_zns');
