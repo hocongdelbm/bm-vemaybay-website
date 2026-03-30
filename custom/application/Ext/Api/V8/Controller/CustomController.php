e63d477c01fb9a5fa0cc1547127478a3802dafa7
ducphamlbm <ducpham@lebaominh.com>
2026-03-30 08:15:09 +0700

    chore: detail bk

--- custom/application/Ext/Api/V8/Controller/CustomController.php
+++ custom/application/Ext/Api/V8/Controller/CustomController.php
@@ -80,7 +80,7 @@ class CustomController extends BaseController
             ) {
                 // Tìm cuộc gọi inbound gần nhất
                 $sql_call = '
-                    SELECT id
+                    SELECT id, name, description, assigned_user_id
                     FROM calls
                     WHERE call_from = "' . $db->quote(trim($booking->phone)) . '"
                         AND direction = "inbound"
@@ -90,17 +90,40 @@ class CustomController extends BaseController
                     ORDER BY date_entered DESC
                     LIMIT 1
                 ';
-                $call_id_autolink = $db->getOne($sql_call);
+                $res_call = $db->query($sql_call);
+                $row_call = $db->fetchByAssoc($res_call);
 
-                if (!empty($call_id_autolink)) {
-                    $db->query('
-                        UPDATE calls
-                        SET booking_id = "' . $db->quote($booking_id) . '"
-                        WHERE id = "' . $db->quote($call_id_autolink) . '"
-                        AND deleted = 0
-                    ');
+                if (!empty($row_call['id'])) {
+                    try {
+                        $db->query('
+                            UPDATE calls
+                            SET booking_id = "' . $db->quote($booking_id) . '"
+                            WHERE id = "' . $db->quote($row_call['id']) . '"
+                            AND deleted = 0
+                        ');
 
-                    $GLOBALS['log']->fatal('DEBUG: Auto-link save_booking (case3) call ' . $call_id_autolink . ' → booking ' . $booking_id . ' (phone: ' . $booking->phone . ') and sql ' . $sql_call);
+                        $bean_note                      = new Note();
+                        $bean_note->id                  = '';
+                        $bean_note->name                = $booking->name;
+                        $bean_note->parent_type         = 'EC_Flight_Bookings';
+                        $bean_note->parent_id           = $booking_id;
+                        $bean_note->description         = trim($row_call['description']) . ' (automap_call_save_bk)';
+                        $bean_note->booking_status      = '8';
+                        $bean_note->assigned_user_id    = $row_call['assigned_user_id'] ?? '';
+                        $bean_note->save();
+
+                        $db->query('
+                            UPDATE ec_flight_bookings
+                            SET booking_status = "6"
+                            WHERE id = "' . $db->quote($booking_id) . '"
+                            AND deleted = 0
+                        ');
+
+                        $GLOBALS['log']->fatal('DEBUG: Auto-link save_booking (case3) call ' . $row_call['id'] . ' → booking ' . $booking_id . ' (phone: ' . $booking->phone . ') and sql ' . $sql_call);
+
+                    } catch (Throwable $th) {
+                        $GLOBALS['log']->fatal("Auto-link saveBK failed: {$th->getMessage()} on line {$th->getLine()} in {$th->getFile()}");
+                    }
                 }
             }
 
