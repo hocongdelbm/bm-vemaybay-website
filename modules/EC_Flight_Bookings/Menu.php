4eee0cbb5f5a6475a65880ee561fd32db70bbd1f
ducphamlbm <ducpham@lebaominh.com>
2026-04-17 14:34:32 +0700

    update check ip login for ctv

--- modules/EC_Flight_Bookings/Menu.php
+++ modules/EC_Flight_Bookings/Menu.php
@@ -60,3 +60,12 @@ if (ACLController::checkAccess('EC_Flight_Bookings', 'list', true) && !isTelesal
 
 if(ACLController::checkAccess('EC_Flight_Bookings', 'list', true))$module_menu[]	= array("index.php?module=EC_Flight_Bookings&action=updateflight&return_module=EC_Flight_Bookings&return_action=updateflight", $mod_strings['LNK_UPDATE_FLIGHT'],"EC_Flight_Bookings", 'EC_Flight_Bookings');
 
+if (is_admin($current_user)) {
+    $module_menu[] = array(
+        "index.php?module=EC_Flight_Bookings&action=telesaleipmgr",
+        "Quản lý Login Telesale",
+        "security-shield",
+        "EC_Flight_Bookings"
+    );
+}
+
