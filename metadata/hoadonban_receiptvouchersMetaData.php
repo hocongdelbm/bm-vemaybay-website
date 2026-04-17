0a425635dbb52aba07a60e670cf114dd88806356
dahyvanlbm <dahyvan@giaonhanh.net>
2026-04-17 16:05:37 +0700

    feat: update hoadonban_receiptvouchers metadata and establish relationships with EC_HoaDonBan and EC_Receipt_Voucher

--- metadata/hoadonban_receiptvouchersMetaData.php
+++ metadata/hoadonban_receiptvouchersMetaData.php
@@ -3,8 +3,8 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$dictionary['hoadon_receiptvouchers'] = array(
-    'table' => 'hoadon_receiptvouchers',
+$dictionary['hoadonban_receiptvouchers'] = array(
+    'table' => 'hoadonban_receiptvouchers',
     'fields' => array(
         array('name' => 'id', 'type' => 'varchar', 'len' => 36),
         array('name' => 'hoadon_id', 'type' => 'varchar', 'len' => 36),
@@ -14,12 +14,12 @@ $dictionary['hoadon_receiptvouchers'] = array(
         array('name' => 'deleted', 'type' => 'bool', 'len' => 1, 'required' => false, 'default' => 0)
     ),
     'indices' => array(
-        array('name' => 'hoadon_receiptvouchers_pk', 'type' => 'primary', 'fields' => array('id')),
-        array('name' => 'idx_hoadon_receiptvouchers_ak', 'type' => 'alternate_key', 'fields' => array('hoadon_id', 'receipt_id')),
-        array('name' => 'idx_hoadon_id_receipt_id_del', 'type' => 'index', 'fields' => array('hoadon_id', 'receipt_id', 'deleted'))
+        array('name' => 'hoadonban_receiptvouchers_pk', 'type' => 'primary', 'fields' => array('id')),
+        array('name' => 'idx_hoadonban_receiptvouchers_ak', 'type' => 'unique', 'fields' => array('hoadon_id', 'receipt_id')),
+        array('name' => 'idx_hoadonban_id_receipt_id_del', 'type' => 'index', 'fields' => array('hoadon_id', 'receipt_id', 'deleted'))
     ),
     'relationships' => array(
-        'hoadon_receiptvouchers' => array(
+        'hoadonban_receiptvouchers' => array(
             'lhs_module' => 'EC_HoaDonBan',
             'lhs_table' => 'ec_hoadonban',
             'lhs_key' => 'id',
@@ -27,7 +27,7 @@ $dictionary['hoadon_receiptvouchers'] = array(
             'rhs_table' => 'ec_receipt_voucher',
             'rhs_key' => 'id',
             'relationship_type' => 'many-to-many',
-            'join_table' => 'hoadon_receiptvouchers',
+            'join_table' => 'hoadonban_receiptvouchers',
             'join_key_lhs' => 'hoadon_id',
             'join_key_rhs' => 'receipt_id'
         )
