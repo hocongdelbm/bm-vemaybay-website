4f070d9bb881ddaa814b9a899e8175e74140c499
dahybeoslbm <dahyv@lebaominh.com>
2026-06-11 16:33:52 +0700

    Enhance booking validation by adding amount and total sell variables in JavaScript and refactor submission checks for better clarity and functionality

--- modules/EC_Receipt_Voucher/js/view.detail.js
+++ modules/EC_Receipt_Voucher/js/view.detail.js
@@ -7,27 +7,44 @@ $(document).ready(function () {
         $('form[name="DetailView"] input:submit[name="Delete"]').remove();
     }
 
-    // Check booking is paid before receipted
-    $('#frmChangeStatus').submit(function () {
-        var booking_id      = $('#frmChangeStatus input:hidden[name="booking_id"]').val();
-        var booking_name    = $('#frmChangeStatus input:hidden[name="booking_name"]').val();
-        var rv_status       = $('#frmChangeStatus input:hidden[name="rv_status"]').val();
-        var is_paid         = 0;
-        var currentTime     = new Date(new Date().toString().split('GMT')[0]+' UTC').toISOString().split('.')[0].replace('T',' ');
+    function checkBeforeSubmit(booking_id, booking_name, rv_status, isAdmin) {
+        if (rv_status == 1 && ['4', '5', '14', '27'].includes(loai_thu) && amount != total_sell) {
+            showToastWarning('Số tiền và tổng giá bán phải bằng nhau!');
+            return false;
+        }
 
         if (booking_id != '' && booking_name != '' && rv_status == 1) {
+            let is_paid = 0;
+            let upd_paid = isAdmin ? '' : '&upd_paid=1';
+
             $.ajax({
                 cache: false,
                 type: 'POST',
-                data: 'booking_id=' + booking_id + '&rv_status=' + rv_status + '&upd_paid=1',
+                data: 'booking_id=' + booking_id + '&rv_status=' + rv_status + upd_paid,
                 url: 'index.php?entryPoint=entryPointCheckBookingPaid',
                 async: false,
                 success: function (output) {
                     is_paid = output;
                 }
             });
-        }
 
+            if (isAdmin && is_paid == 0) {
+                showToastWarning('Booking ' + booking_name + ' chưa nhấn đã thanh toán.');
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Check booking is paid before receipted
+    $('#frmChangeStatus').submit(function () {
+        var booking_id      = $('#frmChangeStatus input:hidden[name="booking_id"]').val();
+        var booking_name    = $('#frmChangeStatus input:hidden[name="booking_name"]').val();
+        var rv_status       = $('#frmChangeStatus input:hidden[name="rv_status"]').val();
+
+        if (!checkBeforeSubmit(booking_id, booking_name, rv_status, false)) {
+            return false;
+        }
     });
 
     // Check booking is paid before receipted
@@ -35,29 +52,9 @@ $(document).ready(function () {
         let booking_id      = $('#frmAdminChangeStatus input:hidden[name="booking_id"]').val();
         let booking_name    = $('#frmAdminChangeStatus input:hidden[name="booking_name"]').val();
         let rv_status       = $('#rv_status :selected').val();
-        let is_paid         = 0;
 
-        if (booking_id != '' && booking_name != '' && rv_status == 1) {
-            $.ajax({
-                cache: false,
-                type: 'POST',
-                data: 'booking_id=' + booking_id + '&rv_status=' + rv_status,
-                url: 'index.php?entryPoint=entryPointCheckBookingPaid',
-                async: false,
-                success: function (output) {
-                    is_paid = output;
-                }
-            });
-            if (is_paid == 0) {
-                $('.toast-warning').addClass('active');
-                $('.toast-warning #toast-content').text('Booking ' + booking_name + ' chưa nhấn đã thanh toán.');
-                $('.toast-warning .progress-bar').animate({ width: "100%" }, 3000);
-                setTimeout(function () {
-                $(".toast-warning").removeClass('active');
-                }, 4000);
-                
-                return false;
-            }
+        if (!checkBeforeSubmit(booking_id, booking_name, rv_status, true)) {
+            return false;
         }
     });
 });
