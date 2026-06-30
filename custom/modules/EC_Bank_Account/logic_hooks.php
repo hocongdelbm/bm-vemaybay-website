3a2da80e552d2cff43bedae09ae4eeef587be2ea
haihugn <hungnh12062000@gmail.com>
2026-06-30 16:19:34 +0700

    feat: clear logic bank list json

--- custom/modules/EC_Bank_Account/logic_hooks.php
+++
@@ -1,6 +0,0 @@
-<?php
-$hook_version = 1;  
-$hook_array = Array();
-$hook_array["after_save"] = Array(); 
-$hook_array["after_save"][] = Array(1, "saveToFileJson", "custom/modules/EC_Bank_Account/EC_Bank_AccountLogicHook.php", "EC_Bank_AccountLogicHook", "saveToFileJson");
-?>
