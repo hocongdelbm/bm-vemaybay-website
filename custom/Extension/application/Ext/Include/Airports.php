290f8d4276d87969b65a8cf32ca18d887448aa28
haihugn <hungnh12062000@gmail.com>
2026-06-30 16:57:39 +0700

    feaT: init modules air manager

---
+++ custom/Extension/application/Ext/Include/Airports.php
@@ -0,0 +1,7 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+$beanList['EC_Airports'] = 'EC_Airports';
+$beanFiles['EC_Airports'] = 'modules/EC_Airports/EC_Airports.php';
+$moduleList[] = 'EC_Airports';
+
+?>
