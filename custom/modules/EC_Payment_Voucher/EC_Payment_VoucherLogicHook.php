ab0848d47eb9683eee2eb1bda5869a3e2b2782dc
haihugn <hungnh12062000@gmail.com>
2026-07-09 17:00:08 +0700

    Feat: recheck deadcode

--- custom/modules/EC_Payment_Voucher/EC_Payment_VoucherLogicHook.php
+++ custom/modules/EC_Payment_Voucher/EC_Payment_VoucherLogicHook.php
@@ -19,15 +19,4 @@ class EC_Payment_VoucherLogicHook
 		// Remove working process
 		myRemoveWorkingProcess($focus->module_dir, $focus->id);
 	}
-
-	// kiểm tra lại tên phiếu, nếu bị trùng thì set lại tên mới
-	function checkVoucherName($focus, $event, $arguments)
-	{
-		$sql = 'SELECT COUNT(id) FROM ec_payment_voucher WHERE name = "' . $focus->name . '"';
-		if ($focus->db->getOne($sql) > 1) {
-			$number = $focus->db->getOne("SELECT COUNT(id) + 1 FROM ec_payment_voucher WHERE DATE_FORMAT(DATE_ADD(date_entered, INTERVAL 7 HOUR), '%Y-%m-%d') = '" . date('Y-m-d') . "'");
-			$new_name = 'PC' . date('ymd') . str_pad($number, 2, 0, STR_PAD_LEFT);
-			$focus->db->query('UPDATE ec_payment_voucher SET name = "' . $new_name . '" WHERE id = "' . $focus->id . '"');
-		}
-	}
 }
