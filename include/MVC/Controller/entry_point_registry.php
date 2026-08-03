b60a058b53fc991e479547c76789108bc159574f
congdeho <congdeho98@gmail.com>
2026-08-03 14:17:01 +0700

    feat: add signed Nextcloud file upload webhook

--- include/MVC/Controller/entry_point_registry.php
+++ include/MVC/Controller/entry_point_registry.php
@@ -113,6 +113,7 @@ $entry_point_registry = array(
     'entryFareSystemWebhook'    => ['file' => 'custom/entrypoints/entryFareSystemWebhook.php', 'auth' => false],
     'entryBookingCreateWebhook' => ['file' => 'custom/entrypoints/entryBookingCreateWebhook.php', 'auth' => false],
     'entryImageUploadWebhook'   => ['file' => 'custom/entrypoints/entryImageUploadWebhook.php', 'auth' => false],
+    'entryFileUploadWebhook'    => ['file' => 'custom/entrypoints/entryFileUploadWebhook.php', 'auth' => false],
     'entryContactWebhook'       => ['file' => 'custom/entrypoints/entryContactWebhook.php', 'auth' => false],
     'entryTelegramWebhook'      => ['file' => 'custom/entrypoints/entryTelegramWebhook.php', 'auth' => false],
     'entryOnepayIPN'            => ['file' => 'custom/entrypoints/entryOnepayIPN.php', 'auth' => false],
