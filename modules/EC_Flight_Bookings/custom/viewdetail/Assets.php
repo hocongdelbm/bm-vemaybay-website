752ebb9796ef8baa191ad2c4cc696c4fb2d030b4
ducphamlbm <ducpham@lebaominh.com>
2026-08-07 13:38:14 +0700

    Update version

--- modules/EC_Flight_Bookings/custom/viewdetail/Assets.php
+++ modules/EC_Flight_Bookings/custom/viewdetail/Assets.php
@@ -16,7 +16,7 @@ trait AssetsTrait
 	private function displayJSTrait() {
 		global $current_user;
 
-		$jsVer = inDeveloperMode() ? time() : '1.3.6';
+		$jsVer = inDeveloperMode() ? time() : '1.3.7';
 
 		$js = '
 			<script src="modules/' . $this->bean->module_dir . '/js/view.detail.js?v=' . $jsVer . '"></script>
