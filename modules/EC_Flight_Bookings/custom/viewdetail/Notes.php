f4820d1fbdcfda370dc09d963b5984f797a563f9
ducphamlbm <ducpham@lebaominh.com>
2026-08-11 13:55:51 +0700

    Remove redundant code

--- modules/EC_Flight_Bookings/custom/viewdetail/Notes.php
+++ modules/EC_Flight_Bookings/custom/viewdetail/Notes.php
@@ -96,7 +96,7 @@ trait NotesTrait {
 					$icon_action = <<<HTML
 						<span class="icon-action">
 							<svg width="16px" height="16px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#808080"><path d="M7 12.5l3 3 7-7" stroke="#808080" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="#808080" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-						</span>'
+						</span>
 					HTML;
 				}
 
